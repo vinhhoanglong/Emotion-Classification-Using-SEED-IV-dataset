11a9182c0b1e1a582ea843cfc694eb3ebc5ba56b
vinhhoanglong <longvinhvhbk@gmail.com>
2024-12-12 21:42:06 +0700

    update

--- architecture.py
+++ architecture.py
@@ -15,7 +15,7 @@ def convlstm2d():
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(256, activation="relu"))
-    model.add(tf.keras.layers.Dense(10))
+    model.add(tf.keras.layers.Dense(4))
     return model
 
 def threedConv():
@@ -32,5 +32,5 @@ def threedConv():
     model.add(tf.keras.layers.BatchNormalization(trainable=False))
     model.add(tf.keras.layers.Activation(activations.relu))
     model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.Dense(10))
+    model.add(tf.keras.layers.Dense(4))
     return model
